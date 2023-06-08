49ce00cf843802f5a7a68cf6d660d23db9a727ae
sinyoung <sinyoung.kim@ntoday.kr>
2023-06-08 22:40:14 +0900

    Initial commit

---
+++ src/main/java/com/sy/rest_api_example/Rest_api_exampleApplication.java
@@ -0,0 +1,13 @@
+package com.sy.rest_api_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rest_api_exampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rest_api_exampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sy/rest_api_example/RestapiexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sy.rest_api_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
